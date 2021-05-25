946e3d3d20f36171f9b38d6439ee5ff4d35f1262
Pradeep Dhatarwal <pradeepdhatarwal@Pradeeps-MacBook-Pro.local>
2021-05-25 23:25:56 +0530

    Added cloudinary image upload and delete

--- routes/upload.js
+++ routes/upload.js
@@ -2,7 +2,8 @@ const router = require("express").Router();
 const cloudinary = require("cloudinary");
 const auth = require("../middlewares/auth");
 const authAdmin = require("../middlewares/authAdmin");
-
+const fs = require("fs");
+const e = require("express");
 //uploading images on cloudinary
 cloudinary.config({
 	cloud_name: process.env.CLOUDINARY_NAME,
@@ -10,37 +11,70 @@ cloudinary.config({
 	api_secret: process.env.CLOUDINARY_API_SECRET,
 });
 
-// upload images
+// Only Admin can upload images
 
-router.post("/upload", (req, res) => {
+router.post("/upload", auth, authAdmin, (req, res) => {
 	try {
 		const { files } = req;
 		if (!files || Object.keys(files).length === 0)
 			return res.status(400).json({ msg: "No files were uploaded" });
-		if (files.file.size > process.env.MAX_UPLOAD_FILE_SIZE)
+		if (files.file.size > process.env.MAX_UPLOAD_FILE_SIZE) {
+			// images should be less than 5MB
+
+			removeTemp(files.file.tempFilePath);
 			return res
 				.status(400)
-				.json({ msg: "Exceeded File size limit, please try again." }); // images should be less than 1MB
+				.json({ msg: "Exceeded File size limit, please try again." });
+		}
 		if (
 			files.file.mimetype !== "image/webp" &&
 			files.file.mimetype !== "image/jpeg" &&
-			files.file.mimetype !== "image/png" 
-		)
+			files.file.mimetype !== "image/png"
+		) {
+			removeTemp(files.file.tempFilePath);
 			return res
 				.status(400)
 				.json({ msg: "Invalid file format, please try again." });
-
-
-				cloudinary.v2.uploader.upload(files.file.tempFilePath, {folder:"test"}, async (err,result)=>{
-					if (err) {
-						throw err;
-					} else {
-						res.json(result); 
-					}
-				} )
+		}
+		cloudinary.v2.uploader.upload(
+			files.file.tempFilePath,
+			{ folder: "test" },
+			async (err, result) => {
+				if (err) {
+					throw err;
+				} else {
+					removeTemp(files.file.tempFilePath);
+					return res.json({
+						public_id: result.public_id,
+						url: result.secure_url,
+					});
+				}
+			}
+		);
+	} catch (err) {
+		removeTemp(files.file.tempFilePath);
+		return res.status(500).json({ msg: err.message });
+	}
+});
+router.post("/destroy",auth, authAdmin, (req, res) => {
+	try {
+		const { public_id } = req.body;
+		if (!public_id) return res.status(400).json({ msg: "No images selected" });
+		cloudinary.v2.uploader.destroy(public_id, (err,result) => {
+			if (err) {
+				throw err;
+			} else {
+				res.json({ msg: "Deleted Image" });
+			}
+		});
 	} catch (err) {
 		return res.status(500).json({ msg: err.message });
 	}
 });
-
+const removeTemp = (path) => {
+	//delete temp folder after uploading
+	fs.unlink(path, (err) => {
+		if (err) throw err;
+	});
+};
 module.exports = router;
